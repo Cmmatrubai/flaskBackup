f7b53bba381ddcfbcdfebc16c5429b714c62e818
Cmmatrubai <chaitanya.matrubai@gmail.com>
2025-04-02 22:26:09 -0400

    Added instructions for setting up the database in schema.sql

--- backend/schema.sql
+++ backend/schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+##run this in terminal
+##mysql -u root -p 
+
+##create database learntrack            
+##use learntrack            
+##source schema.sql            
+##exit  
